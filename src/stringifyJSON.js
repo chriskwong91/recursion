a28cbae5432c4746461514fa3d9494e5edf1dd21
Christopher Wong <chriskwong91@gmail.com>
2016-07-24 15:16:16 -0700

    Completed stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,37 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // handles obj values that are not arrays or objects
+  if (typeof obj === "string") {
+  	return "\"" + obj + "\"";
+  }
+  else if (obj === null) {
+  	return "null";
+  }
+  else if (typeof obj === "number" || typeof obj === "boolean") {
+  	return obj.toString();
+  }
+
+  // If the object is array, iterate through the length and run stringifyJSON on it.
+  if (Array.isArray(obj)) {
+  	var array = [];
+  	for (var i = 0; i < obj.length; i++) {
+  		array.push(stringifyJSON(obj[i]));
+  	}
+  	return "[" + array.join(",") + "]";
+  }
+
+  // If the object is an object, iterate through the length and run stringifyJSON on it.
+  // Also check the object values to ensure they are valid.
+  if (typeof obj === 'object') {
+  	var object = [];
+  	for (var prop in obj) {
+  	  //Check to see if value is undefined or a function. Skip if it is.
+  	  if (obj[prop] === undefined || typeof obj[prop] === "function") {
+  	  	continue;
+  	  }
+  	  object.push(stringifyJSON(prop) + ":" + stringifyJSON(obj[prop]));
+  	}
+  	return "{" + object.join(",") + "}";
+  }
 };
