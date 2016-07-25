e8f0e6ce3a7a1f31e5e5e96de8f7c9938aa61983
Christopher Wong <chriskwong91@gmail.com>
2016-07-24 17:13:53 -0700

    completed getElementsByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,26 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  // Array to store elements
+  var elements = [];
+
+  // set initial node to document.body
+  var node = document.body;
+  
+  function getElements(node) {
+  	// if node contains class, add node to element list
+  	if (node.classList !== undefined && node.classList.contains(className)) {
+  		elements.push(node);
+  	}
+
+  	// check to seee if there are child nodes
+  	if (node.hasChildNodes()) {
+  		// iterate through the childnodes and check classes through recursion
+  	  for (var i = 0; i < node.childNodes.length; i++) {
+  	    getElements(node.childNodes[i]);
+  	  }
+  	}
+  }
+  getElements(node);
+  return elements;
 };
